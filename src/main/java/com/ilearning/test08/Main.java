6aa6cd3527e033cc0e80565b38d3b6d6f0c7935d
xusenlin WX1352635 <xusenlin@h-partners.com>
2024-09-08 15:58:08 +0800

    ilearning--test08

---
+++ src/main/java/com/ilearning/test08/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.ilearning.test08;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputStr = "a2CB1c";
+        Solution solution = new Solution();
+        String ans = solution.characterSort(inputStr);
+        System.out.println(ans);
+    }
+}
